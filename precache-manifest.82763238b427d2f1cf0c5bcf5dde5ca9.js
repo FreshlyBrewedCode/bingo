808d1ac610a438a3eb35a6dfa701e70c149ebcc1
FreshlyBrewedCode <11772241+FreshlyBrewedCode@users.noreply.github.com>
2020-10-16 15:29:12 +0200

    Updates

--- precache-manifest.82763238b427d2f1cf0c5bcf5dde5ca9.js
+++ precache-manifest.82763238b427d2f1cf0c5bcf5dde5ca9.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ade7ca48c64b7cf76fa21762665f8130",
+    "revision": "4acd86b3ec726e79ea8b2673a75d8937",
     "url": "/bingo/index.html"
   },
   {
-    "revision": "e5333603207ec66bb3b0",
+    "revision": "ad6aeae0deeffefc35ea",
     "url": "/bingo/static/css/main.206f1ab2.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/bingo/static/js/2.bcad1da5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e5333603207ec66bb3b0",
-    "url": "/bingo/static/js/main.b41cae43.chunk.js"
+    "revision": "ad6aeae0deeffefc35ea",
+    "url": "/bingo/static/js/main.2806c3e0.chunk.js"
   },
   {
     "revision": "c9787d242660ef1b686b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bingo/precache-manifest.57b006c5bba54616a93b4b0b988bc7c4.js"
+  "/bingo/precache-manifest.82763238b427d2f1cf0c5bcf5dde5ca9.js"
 );
 
 self.addEventListener('message', (event) => {
